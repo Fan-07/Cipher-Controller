b4e15a9cddf597a0b1736a17f3ca25b5b7288f17
Fawn <kuikelprabhav@gmail.com>
2026-06-13 21:15:47 +0545

    Implement Base100 encryption and decryption functions

---
+++ (optional) Base100.lua
@@ -0,0 +1,20 @@
+local Base100 = {}
+
+function Base100.Encrypt(text)
+	local result = {}
+	for i = 1, #text do
+		table.insert(result, "[" .. string.byte(text:sub(i,i)) .. "]")
+	end
+	return table.concat(result, "")
+end
+
+function Base100.Decrypt(text)
+	local result = ""
+	for num in text:gmatch("%[(%d+)%]") do
+		local n = tonumber(num)
+		if n then result = result .. string.char(n) end
+	end
+	return result
+end
+
+return Base100
