2b6895b8f5b84fc2e9a865495f779814833b6245
Fawn <kuikelprabhav@gmail.com>
2026-06-13 21:16:19 +0545

    Implement Base16 encryption and decryption functions

---
+++ (optional) Base16.lua
@@ -0,0 +1,22 @@
+local Base16 = {}
+
+function Base16.Encrypt(text)
+	local result = {}
+	for i = 1, #text do
+		table.insert(result, string.format("%02X", string.byte(text:sub(i, i))))
+	end
+	return table.concat(result, " ")
+end
+
+function Base16.Decrypt(text)
+	local result = ""
+	for word in text:gmatch("%S+") do
+		local n = tonumber(word, 16)
+		if n then
+			result = result .. string.char(n)
+		end
+	end
+	return result
+end
+
+return Base16
